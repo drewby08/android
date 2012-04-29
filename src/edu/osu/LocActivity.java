c673235039d419b2b5d7e651d4904e606410b02a
unknown <drew@drew-DEV.(none)>
2012-04-29 15:39:34 -0400

    added + - zoom buttons
    Signed-off-by: unknown <drew@drew-DEV.(none)>

--- src/edu/osu/LocActivity.java
+++ src/edu/osu/LocActivity.java
@@ -1,15 +1,25 @@
 package edu.osu;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class LocActivity extends MapActivity{
+	
+	LinearLayout linearLayout;
+	MapView mapView;
+	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.whereami);
+        
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+
     }
 
 	@Override
