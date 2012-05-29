7f062e615eb89a54cc4b4de41bd2262b3c6e8cd0
Mike Kusold <kusold@gmail.com>
2012-05-29 15:42:36 -0400

    Deleted files no longer used.

--- src/edu/osu/CheckedInComparator.java
+++
@@ -1,9 +0,0 @@
-package edu.osu;
-
-import java.util.Comparator;
-
-public class CheckedInComparator implements Comparator<FourSquareVenue> {
-    public int compare(FourSquareVenue venue1, FourSquareVenue venue2) {
-        return venue1.herenow - venue2.herenow;
-    }
-}
