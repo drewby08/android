eb4fbdbcae7b890d228e49372d2b47f6f78123f2
Mike Kusold <kusold@gmail.com>
2012-05-28 23:52:10 -0400

    Created an object to hold Foursquare Venues

---
+++ src/edu/osu/FourSquareVenue.java
@@ -0,0 +1,14 @@
+package edu.osu;
+
+import android.location.Location;
+
+public class FourSquareVenue {
+    public String id;
+    public String name;
+    public String address;
+    public String type;
+    public Location location;
+    public int direction;
+    public int distance;
+    public int herenow;
+}
