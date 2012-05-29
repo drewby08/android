eb8141c9da071bee08e8c969e77401af407c74ed
Mike Kusold <kusold@gmail.com>
2012-05-28 22:49:54 -0400

    Created LocationOverlay - To be used for pulling in API

---
+++ src/edu/osu/LocationOverlay.java
@@ -0,0 +1,11 @@
+package edu.osu;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mike
+ * Date: 5/28/12
+ * Time: 10:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LocationOverlay {
+}
