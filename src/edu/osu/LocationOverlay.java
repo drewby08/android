2678536390f681f17e8b70b4a4d4c9f08e6b2114
Mike Kusold <kusold@gmail.com>
2012-05-29 12:24:22 -0400

    Currently this code does not load foursquare locations, but it doesn't crash the app either.
    Instead it gracefully displays a toast notification saying that the
    bars were not found.

--- src/edu/osu/LocationOverlay.java
+++ src/edu/osu/LocationOverlay.java
@@ -1,11 +1,145 @@
 package edu.osu;
 
-/**
- * Created with IntelliJ IDEA.
- * User: mike
- * Date: 5/28/12
- * Time: 10:41 PM
- * To change this template use File | Settings | File Templates.
- */
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
+import com.google.android.maps.GeoPoint;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 public class LocationOverlay {
+
+    /**
+     * Callback url, as set in 'Manage OAuth Costumers' page (https://developer.foursquare.com/)
+     */
+    public static final String CALLBACK_URL = "BeerBuddy://connect";
+    private static final String AUTH_URL = "https://foursquare.com/oauth2/authenticate?response_type=code";
+    private static final String TOKEN_URL = "https://foursquare.com/oauth2/access_token?grant_type=authorization_code";
+    private static final String API_URL = "https://api.foursquare.com/v2";
+
+    private static final String TAG = "FoursquareApi";
+
+    private static final String accessToken = "O42ODDRXG3EHFKRJ0JSKSUQOZLHXQO3GY4JFSZNNM245XDIR";
+
+    ArrayList<FourSquareVenue> mNearbyList = new ArrayList<FourSquareVenue>();
+    private void queryFoursquare(String longitude, String latitude) {
+        String query = "https://api.foursquare.com/v2/venues/search?ll="+ latitude + "," + longitude + "&limit=30&query=nightlife&oauth_token=O42ODDRXG3EHFKRJ0JSKSUQOZLHXQO3GY4JFSZNNM245XDIR&v=20120528";
+    }
+
+//    private void loadNearbyPlaces(final double latitude, final double longitude) {
+//
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                int what = 0;
+//
+//                try {
+//                    mNearbyList = getNearby(latitude, longitude);
+//                } catch (Exception e) {
+//                    what = 1;
+//                    e.printStackTrace();
+//                }
+//            }
+//        }.start();
+//    }
+
+    public ArrayList<FourSquareVenue> getNearby(double latitude, double longitude) throws Exception {
+        ArrayList<FourSquareVenue> venueList = new ArrayList<FourSquareVenue>();
+
+        try {
+            String ll 	= String.valueOf(latitude) + "," + String.valueOf(longitude);
+            URL url 	= new URL(API_URL + "/venues/search?ll=" + ll + "&limit=30&query=nightlife&oauth_token=" + accessToken);
+
+            Log.d(TAG, "Opening URL " + url.toString());
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+
+            urlConnection.connect();
+
+            String response		= streamToString(urlConnection.getInputStream());
+            JSONObject jsonObj 	= (JSONObject) new JSONTokener(response).nextValue();
+
+            JSONArray groups	= (JSONArray) jsonObj.getJSONObject("response").getJSONArray("groups");
+
+            int length			= groups.length();
+
+            if (length > 0) {
+                for (int i = 0; i < length; i++) {
+                    JSONObject group 	= (JSONObject) groups.get(i);
+                    JSONArray items 	= (JSONArray) group.getJSONArray("items");
+
+                    int ilength 		= items.length();
+
+                    for (int j = 0; j < ilength; j++) {
+                        JSONObject item = (JSONObject) items.get(j);
+
+                        FourSquareVenue venue 	= new FourSquareVenue();
+
+                        venue.id 		= item.getString("id");
+                        venue.name		= item.getString("name");
+
+                        JSONObject location = (JSONObject) item.getJSONObject("location");
+
+                        Location loc 	= new Location(LocationManager.GPS_PROVIDER);
+
+                        loc.setLatitude(Double.valueOf(location.getString("lat")));
+                        loc.setLongitude(Double.valueOf(location.getString("lng")));
+
+                        venue.location	= loc;
+                        venue.address	= location.getString("address");
+                        venue.distance	= location.getInt("distance");
+                        venue.herenow	= item.getJSONObject("hereNow").getInt("count");
+                        venue.type		= group.getString("type");
+
+                        venueList.add(venue);
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            throw ex;
+        }
+
+        return venueList;
+    }
+
+    private String streamToString(InputStream is) throws IOException {
+        String str  = "";
+
+        if (is != null) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            try {
+                BufferedReader reader 	= new BufferedReader(new InputStreamReader(is));
+
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                reader.close();
+            } finally {
+                is.close();
+            }
+
+            str = sb.toString();
+        }
+
+        return str;
+    }
 }
--- src/edu/osu/MapActivity.java
+++ src/edu/osu/MapActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import android.content.Context;
@@ -14,10 +15,13 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.provider.Settings;
 import android.util.Log;
+import android.widget.Toast;
 import com.google.android.maps.*;
 import android.widget.LinearLayout;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MapActivity extends com.google.android.maps.MapActivity {
@@ -49,6 +53,23 @@ public class MapActivity extends com.google.android.maps.MapActivity {
         });
         myLocationOverlay.enableMyLocation();
         mapView.getOverlays().add(myLocationOverlay);
+
+        // Add Foursquare Locations
+        Drawable markerDefault = this.getResources().getDrawable(R.drawable.food_biergarten);
+        VenueOverlay venueOverlay = new VenueOverlay(markerDefault);
+        try{
+            ArrayList<FourSquareVenue> venues = new LocationOverlay().getNearby(myLocationOverlay.getMyLocation().getLatitudeE6(), myLocationOverlay.getMyLocation().getLongitudeE6());
+            FourSquareVenue place = new FourSquareVenue();
+            //Collections.sort(venues, CheckedInComparator());
+            while (venues.size() > 0) {
+                place = venues.remove(0);
+                venueOverlay.addOverlayItem((int) (place.location.getLatitude()*1E6),(int) (place.location.getLongitude() * 1E6),place.name, markerDefault);
+            }
+            mapView.getOverlays().add(venueOverlay);
+        } catch (Exception e) {
+            Toast.makeText(MapActivity.this, "Unable to load Bars", Toast.LENGTH_LONG).show();
+        }
+
         mapView.postInvalidate();
 
         mapViewController = mapView.getController();
@@ -104,4 +125,47 @@ public class MapActivity extends com.google.android.maps.MapActivity {
         super.onPause();
         myLocationOverlay.disableMyLocation();
     }
+
+    private class VenueOverlay extends ItemizedOverlay<OverlayItem> {
+
+        private List<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+
+        public VenueOverlay(Drawable defaultMarker) {
+            super(boundCenterBottom(defaultMarker));
+        }
+
+        public void addOverlayItem(int lat, int lon, String title, Drawable altMarker) {
+            GeoPoint point = new GeoPoint(lat, lon);
+            OverlayItem overlayItem = new OverlayItem(point, title, null);
+            addOverlayItem(overlayItem, altMarker);
+        }
+
+        public void addOverlayItem(OverlayItem overlayItem) {
+            mOverlays.add(overlayItem);
+            populate();
+        }
+
+        public void addOverlayItem(OverlayItem overlayItem, Drawable altMarker) {
+            overlayItem.setMarker(boundCenterBottom(altMarker));
+            addOverlayItem(overlayItem);
+        }
+
+        @Override
+        protected OverlayItem createItem(int i) {
+            return mOverlays.get(i);
+        }
+
+        @Override
+        public int size() {
+            return mOverlays.size();
+        }
+
+        @Override
+        protected boolean onTap(int index) {
+            Toast.makeText(MapActivity.this, getItem(index).getTitle(), Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+    }
+
 }
